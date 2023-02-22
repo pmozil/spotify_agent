6eb0139aba53d3d5a4b7e944db15a024d040de44
Petro Mozil <mozil.petryk@gmail.com>
2023-02-22 13:44:22 +0200

    Add task 2

--- task_2.py
+++ task_2.py
@@ -1,3 +1,5 @@
+"""The spotify user agent"""
+from typing import Any
 import requests
 import dotenv
 import os
@@ -43,3 +45,37 @@ def get_artist_by_id(artist_id: str) -> dict:
     )
 
     return artist_response.json()
+
+def parse_json(data: dict | list | Any, *, parent={}, print_data: bool = True):
+    """
+    Print the receieved json
+
+    Args:
+        data: dict - the json
+    """
+    if isinstance(data, list):
+        for elem in data:
+            print(f"- {elem}")
+        return
+    elif isinstance(data, dict):
+        print("Here are the available keys:")
+        if print_data:
+            for key, _ in data.items():
+                print(f"{key}")
+        next_entry = input("Which entry would you like to see?\n")
+        if next_entry == ".." and parent:
+            parse_json(parent)
+            return
+        if next_entry in data:
+            parse_json(data[next_entry], parent=data)
+            return
+        print("No such entry in json. Please reenter the entry")
+        parse_json(data, parent=parent)
+        return
+    print(data)
+
+if __name__ == "__main__":
+    artist_id = input("Please enter the artist id:\n")
+    artist_json = get_artist_by_id(artist_id)
+    print(artist_json)
+    parse_json(artist_json)
